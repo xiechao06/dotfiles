350513446176fbbb89879e14b357deb6bd555744
xc <xiechao06@gmail.com>
2017-12-25 10:05:09 +0800

    add fzf

--- install.sh
+++ install.sh
@@ -11,8 +11,10 @@ ln -sfv "$DOTFILES_DIR/gitconfig" ~/.gitconfig
 ln -sfv "$DOTFILES_DIR/i3status.conf" ~/.i3status.conf
 ln -sfv "$DOTFILES_DIR/vim" ~/.vim
 
+git submodule update --init --recursive
+
+source "$DOTFILES_DIR/install/apt-use-mirror.sh"
 sudo apt update
 sudo apt install -y vim inkscape zathura spacefm silversearcher-ag tmuxinator i3 git
 
-source "$DOTFILES_DIR/install/apt-use-mirror.sh"
-source "$DOTFILES_DIR/install/fzf.sh"
+source "$DOTFILES_DIR/install/fzf"
