486f69f0e8bb7bbd46bbdf2fbb0abfe8a800b429
xiechao06 <xiechao06@gmail.com>
2017-12-29 15:56:05 +0800

    setup xautolock

--- do-install.sh
+++ do-install.sh
@@ -10,6 +10,7 @@ echo $DOTFILES_DIR
 ln -sfv "$DOTFILES_DIR/gitconfig" ~/.gitconfig
 ln -sfv "$DOTFILES_DIR/i3status.conf" ~/.i3status.conf
 ln -sfv "$DOTFILES_DIR/tmux.conf" ~/.tmux.conf
+ln -sfv "$DOTFILES_DIR/xinitrc" ~/.xinitrc
 
 git submodule update --init --recursive --remote
 [ ! -d ~/.vim ] && ln -sfv "$DOTFILES_DIR/.cache/vim" ~/.vim
@@ -20,7 +21,7 @@ if [ -n "$(getent group sudo | grep $(whoami))" ]; then
     sudo apt update
     sudo apt install -y \
         vim inkscape zathura spacefm silversearcher-ag tmuxinator i3 \
-        git postgresql feh
+        git postgresql feh xautolock
 fi
 
 . "$DOTFILES_DIR/install/fzf"
